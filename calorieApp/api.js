074e7818a155ec7cd66d785369c0d70dc51e61b8
rajmahal4 <rsingh6@mail.sfsu.edu>
2021-09-25 18:55:49 -0700

    new Errors

--- calorieApp/api.js
+++ calorieApp/api.js
@@ -1,11 +1,25 @@
 const API_KEY = 'UGmZY9ss0Ig2sM0eoBhEIhzJ7ihYmxtfI9lH8eyJ'
 let data;
+import fetch from "node-fetch";
 
-async function findFood(q){
-    if(q == "" || q == null) return
-    const response = await fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key='+`${API_KEY}`+'&query='+`${q}`, {mode: 'cors'});
-    const data = await response.json();
-    return data
-}
+//  module.exports.findFood = async function(q){
+//      await findFood(q)
+//  }
 
-module.exports = findFood
+ async function findFood(q){
+     // if(q == "" || q == null) return
+     q = 'cheese'
+     const response = await fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key='+`${API_KEY}`+'&query='+`${q}`, {mode: 'cors'});
+     const data = await response.json();
+     return data
+ }
+
+// module.exports = {
+//     findFood: async (q) =>{
+//         q = 'cheese'
+//         const response = await fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key='+`${API_KEY}`+'&query='+`${q}`, {mode: 'cors'});
+//         const data = await response.json();
+//         return data
+//     }
+// }
+module.exports = findFood()
--- calorieApp/routes/users.js
+++ calorieApp/routes/users.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const findFood = require('../api')
+const findFood= require('../api')
+// const fetch = require('node-fetch')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/food', function(req, res, next) {
-  console.log(findFood('cheese'))
-});
+router.get('/food', function(req,res, next){
+  console.log(findFood())
+  next()
+})
 
-
-module.exports = router;
+module.exports = router
